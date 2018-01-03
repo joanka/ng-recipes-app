8ed3ff12cdd639bc36142602180665c63de27a61
joanka <joasiek136@gmail.com>
2018-01-04 00:27:59 +0100

    added header navigation with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  loadedItem = 'recipe';
+  
+  onNavigate(item: string) {
+    this.loadedItem = item;
+  }
 }
