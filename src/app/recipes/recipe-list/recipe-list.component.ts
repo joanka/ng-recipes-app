7b2920e845187dd9ef3ac3646550a4f61bae26ba
joanka <joasiek136@gmail.com>
2018-01-05 15:03:51 +0100

    passed recipe data with property and event binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import {Recipe} from '../recipe.model';
 
@@ -8,16 +8,21 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeFinallySelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg')
+    new Recipe('A test recipe-1', 'This is simply a first test', 'http://maxpixel.freegreatpicture.com/static/photo/2x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'),
+    new Recipe('A test recipe-2', 'This is simply a second test', 'http://maxpixel.freegreatpicture.com/static/photo/2x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'),
+    new Recipe('A test recipe-3', 'This is simply a third test', 'http://maxpixel.freegreatpicture.com/static/photo/2x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedRecipe(recipe: Recipe) {
+    this.recipeFinallySelected.emit(recipe);
   }
 
 }
