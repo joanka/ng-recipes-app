6c163fc52f945337878f7e52238ec92b9b97dabc
joanka <joasiek136@gmail.com>
2018-03-01 00:17:17 +0100

    used Form to add shopping list items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -15,7 +15,7 @@ export class ShoppingListService {
     }
 
     addIngredient(ingredient: Ingredient) {
-        this.ingredients.push(ingredient);
+        this.ingredients.push(ingredient); 
         this.ingredientsChanged.emit(this.ingredients.slice());
     }
 
